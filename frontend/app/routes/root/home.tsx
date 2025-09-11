9c851c6db500ea319e25111b3a0771a4d97df794
A1n2F <alissonfferreira22@gmail.com>
2025-09-11 11:24:22 -0300

    Project Config

---
+++ frontend/app/routes/root/home.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { Route } from "../../+types/root";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "TaskHub" },
+    { name: "description", content: "Welcome to TaskHub!" },
+  ];
+}
+
+const Homepage = () => {
+    return (
+        <div>
+            Homepage
+        </div>
+    )
+}
+
+export default Homepage
