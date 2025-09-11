f55d33406452b66ce785e065c41bd5773b63884c
A1n2F <alissonfferreira22@gmail.com>
2025-09-11 11:40:33 -0300

    Auth Routes with react-router

--- frontend/app/routes/root/home.tsx
+++ frontend/app/routes/root/home.tsx
@@ -12,8 +12,18 @@ export function meta({}: Route.MetaArgs) {
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
+        <div className="w-full h-screen flex items-center justify-center gap-4">
+            <Link to="/sign-in">
+                <Button className="bg-blue-500 text-white">
+                    Login
+                </Button>
+            </Link>
+
+            <Link to="/sign-up">
+                <Button variant="outline" className="bg-blue-500 text-white">
+                    Sign Up
+                </Button>
+            </Link>
         </div>
     )
 }
